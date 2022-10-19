374f7c5cf9525cb04c4fc076cdc035f4f1cfcdab
christyuda_ <53987991+christyuda@users.noreply.github.com>
2022-10-19 16:01:47 +0700

    tugas kedua 2

---
+++ website/website/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context = {
+        'Judul' : 'blog1',
+        'h1': 'Django'
+    }
+    return render(request,'wiw.html',context)
+
+def recent(request):
+    context = {
+        'Judul' : 'blog2',
+        'h1' : 'Python'
+    } 
+    return render (request,'blog/wiw.html', context)
