74021c6c5a52ac405f0159f6a6beb88eae7222ff
christyuda_ <53987991+christyuda@users.noreply.github.com>
2022-11-02 15:46:45 +0700

    pertemuan 7

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('blog/', views.index, name='about'),
     path('blog/', include('blog.urls'))
 ]
